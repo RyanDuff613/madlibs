958ecebcce9d6796ab22d2705c53bd0a2745f979
Ryan Duff <ryanduff613@gmail.com>
2020-03-31 12:11:52 -0700

    for loop written but not working, may be improperly inserting blanks[]

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,16 @@ $(document).ready(function() {
     
     var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    blanks.forEach(function(blank) {
+    /*  blanks.forEach(function(blank) {
       var userInput = $("input#" + blank).val();
       $("." + blank).text(userInput);
-    });
+    }); */
+    console.log(blanks.length);
+
+    for(index = 0, index<blanks.length, index+=1){
+      var userInput = $('input#'+blanks[index]).val();
+      $("." + blanks[index]).text(userInput);
+    }
 
     $("#story").show();
 
